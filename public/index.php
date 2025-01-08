62cd2cb1f8ba3196cfc126fb673b292689c14e1e
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-08 22:25:23 +0100

    feat: add class Router with basic method

--- public/index.php
+++ public/index.php
@@ -1,7 +1,36 @@
 <?php
+
+use HubCook\Core\Router;
+
 const BASE_PATH = __DIR__ . "/../";
 
 //import autload
 require BASE_PATH . "vendor/autoload.php";
 
-echo "test";
+
+
+function printAndDie(mixed $value):void
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
+}
+
+function printValue(mixed $value):void
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+$url = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+$router = new Router();
+
+echo "uri";
+
+$router->addGetRoute('/', 'homepage');
+printValue($router->getRoutes());
+
+$router->routeTo($url, $method);
