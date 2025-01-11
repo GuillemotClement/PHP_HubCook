5bd2afc27691d70773b46e8f1e6def260cacd51b
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-11 17:59:37 +0100

    style: add homepage

--- src/Template/Pages/Homepage.view.php
+++ src/Template/Pages/Homepage.view.php
@@ -1,1 +1,19 @@
-<h1>Homepage</h1>
+<div class="hero ">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src="https://plus.unsplash.com/premium_photo-1661777727576-98bcc686aec4?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Hubcook</h1>
+      <div class="py-6">
+        <p class="py-6">
+          Simplifiez votre quotidien culinaire avec HubCook, votre assistant personnel pour la planification des repas
+        <p class="">
+          Fini le casse-tête du "Qu'est-ce qu'on mange ce soir ?" ! Notre application vous permet de composer vos
+          menus pour la semaine en quelques clics, grâce à une bibliothèque de recettes variées et inspirantes.
+        </p>
+      </div>
+      <button class="btn btn-primary">Nos recettes</button>
+    </div>
+  </div>
+</div>
